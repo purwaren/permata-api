b3c01ce9b44828f03e09d4d722fba89d08124c40
purwaren <and.thau@gmail.com>
2020-03-24 07:37:41 +0700

    add dto validation

--- dto_va.go
+++ dto_va.go
@@ -1,7 +1,15 @@
 package permata
 
+import validation "github.com/go-ozzo/ozzo-validation"
+
 type GetBillRq struct {
     GetBillRq BillReq
+}
+
+func (p GetBillRq) Validate() error {
+    return validation.ValidateStruct(&p,
+        validation.Field(&p.GetBillRq, validation.Required),
+    )
 }
 
 type BillReq struct {
@@ -10,6 +18,22 @@ type BillReq struct {
     ViTraceNo   string  `json:"VI_TRACENO"`
     ViTrnDate   string  `json:"VI_TRNDATE"`
     ViDelChnl   string  `json:"VI_DELCHANNEL"`
+}
+
+func (p BillReq) Validate() error {
+    return validation.ValidateStruct(&p,
+        validation.Field(&p.InstCode, validation.Required),
+        validation.Field(&p.ViVaNumber, validation.Required),
+        validation.Field(&p.ViTraceNo, validation.Required),
+        validation.Field(&p.ViTrnDate, validation.Required),
+        validation.Field(&p.ViDelChnl, validation.Required),
+    )
+}
+
+func (p BillReq) ValidateTrnDate() error {
+    return validation.ValidateStruct(&p,
+        validation.Field(&p.ViTrnDate, validation.Date("2006-01-02T15:04:05+07:00")),
+    )
 }
 
 type GetBillRs struct {
@@ -27,8 +51,14 @@ type PayBillRq struct {
     PayBillRq PayBillReq
 }
 
+func (p PayBillRq) Validate() error {
+    return validation.ValidateStruct(&p,
+        validation.Field(&p.PayBillRq, validation.Required),
+    )
+}
+
 type PayBillReq struct {
-    InstCode    string `json:"INSTCODE"`
+    InstCode    string  `json:"INSTCODE"`
     ViVaNumber  string  `json:"VI_VANUMBER"`
     ViTraceNo   string  `json:"VI_TRACENO"`
     ViTrnDate   string  `json:"VI_TRNDATE"`
@@ -36,6 +66,24 @@ type PayBillReq struct {
     ViCcy       string  `json:"VI_CCY"`
     ViDelChnl   string  `json:"VI_DELCHANNEL"`
     RefInfo     []RefInfo `json:"RefInfo"`
+}
+
+func (p PayBillReq) Validate() error {
+    return validation.ValidateStruct(&p,
+        validation.Field(&p.InstCode, validation.Required),
+        validation.Field(&p.ViVaNumber, validation.Required),
+        validation.Field(&p.ViTraceNo, validation.Required),
+        validation.Field(&p.ViTrnDate, validation.Required),
+        validation.Field(&p.BillAmount, validation.Required),
+        validation.Field(&p.ViCcy, validation.Required),
+        validation.Field(&p.ViDelChnl, validation.Required),
+    )
+}
+
+func (p PayBillReq) ValidateTrnDate() error {
+    return validation.ValidateStruct(&p,
+        validation.Field(&p.ViTrnDate, validation.Date("2006-01-02T15:04:05+07:00")),
+    )
 }
 
 type RefInfo struct {
