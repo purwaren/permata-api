c9a6e25934e8e2e8c485edd75d75dcdfcefe351b
kedifeles <kedifeleskatten@pm.me>
2020-03-29 13:29:25 +0700

    config: renamed

--- config.go
+++ config.go
@@ -2,11 +2,11 @@ package permata
 
 // Config is config for access Permata API
 type Config struct {
-	URL       string
-	APIKey    string
-	ClientID  string
-	ClientSecretKey  string
-	ClientStaticKey string
+	URL          string
+	APIKey       string
+	StaticKey    string
+	ClientID     string
+	ClientSecret string
 
 	LogPath string
 }
